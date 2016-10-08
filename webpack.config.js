80c63b8385926dc66e48c9e8b49398d9dfdbcd9c
Alex Bennett <alexandergraybennett@gmail.com>
2016-10-08 14:36:03 -0600

    handling front-end envs

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,16 @@ module.exports = {
       {
         test: /\.js/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
       }
     ]
+  },
+  node: {
+    fs: "empty",
+    child_process: 'empty'
   },
   plugins: [
     new DotenvPlugin({
